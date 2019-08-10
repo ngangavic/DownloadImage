5933cff87c133fc8d450143f89c712f103874ff2
ngangavic <ngangavictor10@gmail.com>
2019-08-10 17:39:42 +0300

    Image circle transformation

--- app/src/main/java/com/example/downloadimage/MainActivity.java
+++ app/src/main/java/com/example/downloadimage/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         buttonDownload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Picasso.get().load(getUrl()).into(imageView);
+                Picasso.get().load(getUrl()).transform(new CircleTransformation()).into(imageView);
             }
         });
     }
